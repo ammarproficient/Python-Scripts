02fdf66a9fddf8c079da8683d5813e14419c62af
ammarproficient <ammarproficient@gmail.com>
2024-06-23 23:52:51 +0500

    added code below to check total number installed packages on your pc

--- total_packages.py
+++ total_packages.py
@@ -19,3 +19,24 @@ with open(output_file, 'w') as f:
         f.write(package.split()[0] + '\n')
 
 print(f"List of installed packages has been written to {output_file}")
+
+
+
+# this below code will print all the packages name with package number
+
+# import subprocess
+#
+# # Run the pip list command and capture the output
+# result = subprocess.run(['pip', 'list'], stdout=subprocess.PIPE, text=True)
+#
+# # Split the output into lines
+# packages = result.stdout.split('\n')
+#
+# # Print the total number of installed packages (excluding the header lines)
+# total_packages = len(packages) - 3
+# print(f"Total number of installed packages: {total_packages}")
+#
+# # Print each package name with its index
+# for index, package in enumerate(packages[2:-1], start=1):  # Skip the header and footer lines
+#     package_name = package.split()[0]  # Extract the package name
+#     print(f"{index}: {package_name}")
